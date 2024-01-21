efdd2b798b34598072e05605e74de5c2f5b47d9f
Vinicius Morais <vinidev.morais@gmail.com>
2024-01-21 13:42:38 -0300

    created class UsedProduct

---
+++ Entities/UsedProduct.cs
@@ -0,0 +1,20 @@
+namespace Imported_Products.Entities
+{
+    public class UsedProduct : Product
+    {
+        public DateTime ManuFactureDate { get; set; }
+        
+        public UsedProduct ()
+        {
+
+        }
+        public UsedProduct (string name, double price, DateTime manufacturedate) : base (name, price )
+        {
+            ManuFactureDate = manufacturedate;
+        }
+        public override string PriceTag()
+        {
+            return Name + " (used)  $" + Price + " (Manufacture date: " +  ManuFactureDate + ")";
+        }
+    }
+}
